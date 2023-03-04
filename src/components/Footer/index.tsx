c816cddaa4c9817600cdb9068278c130351e64aa
joseeugeniio <jeugenioneves@gmail.com>
2023-03-03 22:45:27 -0300

    config normal

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return <>
-    <div className="container mx-auto flex flex-row justify-between pb-10 px-56">
+    <div className="container mx-auto text-center flex flex-col justify-between pb-10 px-4">
         <p className="text-stone-500">© 2023 José Eugênio</p>
         <span className="text-stone-500">Última Atualização: 03-03-2023 22:26</span>
     </div>
